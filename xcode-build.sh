76b7b824252aaf797cb7d5ff353afc88039c70eb
Alex Malyutin <alex.malyutin.dev@gmail.com>
2023-10-19 21:19:58 +0400

    wip 3

--- xcode-build.sh
+++ xcode-build.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-cmake -G "Xcode" -T buildsystem=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release ..
-xcodebuild -workspace GameNetworkingSockets.xcodeproj/project.xcworkspace -scheme GameNetworkingSockets
-xcodebuild -project GameNetworkingSockets.xcodeproj -target GameNetworkingSockets -sdk macosx -configuration Release clean build
+# cmake -G "Xcode" -T buildsystem=1 -DCMAKE_BUILD_TYPE=Release -DCMAKE_CONFIGURATION_TYPES=Release ..
+# xcodebuild -workspace GameNetworkingSockets.xcodeproj/project.xcworkspace -scheme GameNetworkingSockets
+# xcodebuild -project GameNetworkingSockets.xcodeproj -target GameNetworkingSockets -sdk macosx -configuration Release clean build
 
 
 cmake -G "Xcode" -T buildsystem=1 \
     -DCMAKE_BUILD_TYPE=Release \
     -DCMAKE_CONFIGURATION_TYPES=Release ..
 
-xcodebuild archive \
+xcodebuild clean archive \
     -project GameNetworkingSockets.xcodeproj \
     -scheme GameNetworkingSocketsFRAMEWORK \
     -destination "generic/platform=macOS" \
